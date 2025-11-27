16874f84f6e7b0ebad43f86bab2420f7a7cdc335
amajalca <196114220+amajalca@users.noreply.github.com>
2025-11-27 03:24:47 -0600

    Refactor platform scripts and fix scene node hierarchy

--- scenes/platforms/scripts/cookie_platform.gd
+++ scenes/platforms/scripts/cookie_platform.gd
@@ -1,1 +1,16 @@
 extends AnimatableBody3D
+
+@export var degrees_per_second: float = 90.0
+@export var randomize_on_ready := true
+@export var min_speed := 60.0
+@export var max_speed := 120.0
+
+func _ready() -> void:
+	if randomize_on_ready:
+		var s = randf_range(min_speed, max_speed)
+		if randf() < 0.5:
+			s = -s
+		degrees_per_second = s
+
+func _physics_process(delta: float) -> void:
+	rotate_y(deg_to_rad(degrees_per_second) * delta)
